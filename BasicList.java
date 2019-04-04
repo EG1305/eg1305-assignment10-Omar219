f5501ec008dfbecdc1c4c53bb01df263f6d3c31a
Omar219 <46610140+Omar219@users.noreply.github.com>
2019-04-04 14:56:12 -0500

    Add files via upload

--- BasicList.java
+++ BasicList.java
@@ -1,8 +1,30 @@
 package Homework;
 
-public class BasicList {
+import java.util.Arrays;
+import java.util.ArrayList;
 
+public class BasicList {
 	public static void main(String[] args) {
-		
+		ArrayList list = new ArrayList(10);
+
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		list.add(50);
+		list.add(60);
+		list.add(70);
+		list.add(80);
+		list.add(90);
+		list.add(100);
+		System.out.println(list);
+		System.out.println("Size of the list is " + list.size());
+
+		list.add(5, 200);
+
+		System.out.println(list);
+		System.out.println("Size of the list is " + list.size());
+		System.out.println("Value of 200 is located at "  + list.indexOf(200));
 	}
+
 }
